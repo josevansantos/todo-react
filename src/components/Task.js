54fcf2d0311dd8991dd887715dd38b06e34da9c8
Josevan Santos <josevansnt@gmail.com>
2021-08-28 17:23:54 -0300

    style: adding color palette

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,12 +4,21 @@ import './Task.css';
 
 const Task = ({ task, handleTaskClick }) => {
   return (
-    <div
-      className="task-container"
-      style={task.completed ? { borderLeft: '6px solid aquamarine' } : {}}
-    >
-      <div className="task-title" onClick={() => handleTaskClick(task.id)}>
-        {task.title}
+    <div>
+      <div
+        className="task-container"
+        style={
+          task.completed
+            ? {
+                borderLeft: '6px solid aquamarine',
+                textDecoration: 'line-through',
+              }
+            : {}
+        }
+      >
+        <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+          {task.title}
+        </div>
       </div>
     </div>
   );
